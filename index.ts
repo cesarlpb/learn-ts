f6ef170182eb7449ea3344e99159489bcb309754
César P. <cesarlpb@gmail.com>
2025-04-01 19:25:31 +0200

    tipos simples en TS

--- index.ts
+++ index.ts
@@ -18,3 +18,50 @@ const usuario: Usuario = {
 }
 
 console.log(usuario)
+
+// Tipos primitivos
+
+let estaLogeado = false;
+// estaLogeado = "falso"
+// no se puede cambiar el tipo del boolean a string y arroja error por defecto
+
+let num = 2;
+let num2 = 3;
+sumar(num, num2);
+
+// Ejemplos:
+let verdadero: boolean;
+verdadero = true;
+// verdadero = "true"; // error
+
+let numero: number;
+numero = 1;
+// numero = "1" // error
+let bigInt: bigint;
+bigInt = 1n;
+// bigInt = 1; // error
+
+let texto: string;
+texto = "Texto!";
+// texto = 1; // error
+
+let objeto: object; // no se suele utilizar
+objeto = {
+    key: "value" 
+}
+// objeto = "objeto"; // error
+
+let symbol: symbol;
+symbol = Symbol("symbol");
+// symbol = "symbol"; // symbol
+
+let miUndefined: undefined;
+miUndefined = undefined;
+// miUndefined = "undefined"; // error
+let nulo: null;
+nulo = null;
+// nulo = "nulo"; // error
+
+let arr: number[]; // array de números
+arr = [1, 2, 3];
+// arr = [1, 2, '3']; // error
